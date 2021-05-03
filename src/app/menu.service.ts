c94da80b1ff3130ea32e836b92e93342fc57329e
Ryan Doering <ryan.doering@ndus.edu>
2021-05-02 19:03:15 -0500

    This is the stucture

---
+++ src/app/menu.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { MenuCategory } from './Model/menu-category';
+import { MenuItem } from './Model/menu-item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+  menuCatigories:MenuCategory[] = []
+  activeCatigory:MenuCategory = new MenuCategory('','','');
+
+  indexOfCatigory:number = 0;
+
+  menuItems:MenuItem[]=[];
+  activeMenuItem:MenuItem = new MenuItem('','','');
+  IndexOfMenuItem:number = 0;
+
+  constructor() { }
+}
